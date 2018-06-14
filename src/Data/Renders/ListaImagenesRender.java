f4682cb3a2bc32699d73016e19bab62cac708b2b
victormartor <31761760+victormartor@users.noreply.github.com>
2018-06-14 14:24:38 +0200

    Depuración de código (renders completados)

--- src/Data/Renders/ListaImagenesRender.java
+++ src/Data/Renders/ListaImagenesRender.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Data.Renders;
 
 import Data.Clases.Imagen;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Image;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
@@ -17,46 +13,58 @@ import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
 /**
+ * Render para que una lista tenga solo imágenes.
  *
  * @author Víctor Martín Torres - 12/06/2018
  */
-public class ListaImagenesRender extends JLabel implements ListCellRenderer{
-
+public class ListaImagenesRender extends JLabel implements ListCellRenderer
+{
+    /**
+     * Personaliza el elemento de la lista cargando la imagen.
+     * @param list Lista
+     * @param value Valor del elemento.
+     * @param index Índice del elemento dentro de la lista.
+     * @param isSelected El elemento está seleccionado.
+     * @param cellHasFocus El elemento tiene el focus.
+     * @return El elemento personalizado.
+     */
     @Override
     public Component getListCellRendererComponent(JList list, Object value, 
-            int index, boolean isSelected, boolean cellHasFocus) {
-        
-        String[] asPartes = value.toString().split(":");
+            int index, boolean isSelected, boolean cellHasFocus) 
+    {
         ImageIcon iconoEscalado = null;
         Imagen imagen = null;
+        
+        //Dividir el elemento en distintas partes separadas por :
+        String[] asPartes = value.toString().split(":");      
+        
+        //Cargar su imagen de la base de datos
         try {
             imagen = new Imagen(Integer.parseInt(asPartes[0]));
-        } catch (Exception ex) {
-            Logger.getLogger(ListaImagenesRender.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NumberFormatException | SQLException ex) {
+            Logger.getLogger(ListaImagenesRender.class.getName())
+                    .log(Level.SEVERE, null, ex);
         }
 
-        try {
-            Image image = new ImageIcon(imagen.getRuta()).getImage();
-            iconoEscalado = new ImageIcon (image.getScaledInstance(-1,100,Image.SCALE_SMOOTH));
-        } catch (Exception ex) {
-            Logger.getLogger(ListaRender.class.getName()).log(Level.SEVERE, null, ex);
+        //Obtener el icono desde el archivo y escalándolo
+        if(imagen != null)
+        {
+           try {
+                Image image = new ImageIcon(imagen.getRuta()).getImage();
+                iconoEscalado = new ImageIcon(
+                        image.getScaledInstance(-1,100,Image.SCALE_SMOOTH));
+            } catch (Exception ex) {
+                Logger.getLogger(ListaRender.class.getName())
+                        .log(Level.SEVERE, null, ex);
+            } 
         }
-
+        
+        //Asignar el icono una vez obtenido
         if(iconoEscalado != null){
             setIcon(iconoEscalado);
         }
-        /*
-        if(isSelected){
-            setBackground(list.getSelectionBackground());
-            setForeground(list.getSelectionForeground());
-        }else{
-            setBackground(list.getBackground());
-            setForeground(list.getForeground());
-        }
-        setEnabled(true);
-        setFont(list.getFont());
-        setOpaque(true);
-        */
+        
+        //PERSONALIZAR COLORES/////////////////////////////////////////
         Color background;
         Color foreground;
 
@@ -64,27 +72,29 @@ public class ListaImagenesRender extends JLabel implements ListCellRenderer{
         JList.DropLocation dropLocation = list.getDropLocation();
         if (dropLocation != null
                 && !dropLocation.isInsert()
-                && dropLocation.getIndex() == index) {
-
+                && dropLocation.getIndex() == index) 
+        {
             background = Color.BLUE;
             foreground = Color.WHITE;
 
-        // check if this cell is selected
-        } else if (isSelected) {
-            background = Color.BLACK;
-            foreground = Color.WHITE;
+            // check if this cell is selected
+            } else if (isSelected) 
+            {
+                background = Color.BLACK;
+                foreground = Color.WHITE;
 
-        // unselected, and not the DnD drop location
-        } else {
-            background = Color.WHITE;
-            foreground = Color.BLACK;
+            // unselected, and not the DnD drop location
+            } else 
+            {
+                background = Color.WHITE;
+                foreground = Color.BLACK;
+            }
+
+            setBackground(background);
+            setForeground(foreground);
+            setOpaque(true);
+
+            return this;
         }
-
-        setBackground(background);
-        setForeground(foreground);
-        setOpaque(true);
-       
-        return this;
-    }
 }
 
--- src/Data/Renders/ListaRender.java
+++ src/Data/Renders/ListaRender.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Data.Renders;
 
 import Data.Clases.Imagen;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Image;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
@@ -17,38 +13,51 @@ import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
 /**
+ * Render para que una lista tenga imágenes y el nombre del elemento.
  *
  * @author Víctor Martín Torres - 12/06/2018
  */
-public class ListaRender extends JLabel implements ListCellRenderer{
-
+public class ListaRender extends JLabel implements ListCellRenderer
+{
+    /**
+     * Personaliza el elemento de la lista cargando la imagen y el nombre.
+     * @param list Lista
+     * @param value Valor del elemento.
+     * @param index Índice del elemento dentro de la lista.
+     * @param isSelected El elemento está seleccionado.
+     * @param cellHasFocus El elemento tiene el focus.
+     * @return El elemento personalizado.
+     */
     @Override
     public Component getListCellRendererComponent(JList list, Object value, 
-            int index, boolean isSelected, boolean cellHasFocus) {
-        
+            int index, boolean isSelected, boolean cellHasFocus) 
+    {   
         ImageIcon iconoEscalado = null;
+        
+        //Dividir el elemento en distintas partes separadas por :
         String[] asPartes = value.toString().split(":");
         
+        //Asignar el texto
         setText(asPartes[1]);
+        
+        //Cargar su imagen de la base de datos y obtener el icono
         int iId_Imagen = Integer.parseInt(asPartes[2]);
         try {
-            Image imagen = new ImageIcon(new Imagen(iId_Imagen).getRuta()).getImage();
-            iconoEscalado = new ImageIcon (imagen.getScaledInstance(-1,100,Image.SCALE_SMOOTH));
-        } catch (Exception ex) {
-            Logger.getLogger(ListaRender.class.getName()).log(Level.SEVERE, null, ex);
+            Image imagen = new ImageIcon(
+                    new Imagen(iId_Imagen).getRuta()).getImage();
+            iconoEscalado = new ImageIcon(
+                    imagen.getScaledInstance(-1,100,Image.SCALE_SMOOTH));
+        } catch (SQLException ex) {
+            Logger.getLogger(ListaRender.class.getName())
+                    .log(Level.SEVERE, null, ex);
         }
 
+        //Asignar el icono una vez obtenido
         if(iconoEscalado != null){
             setIcon(iconoEscalado);
         }
-        /*
-        if(isSelected){
-            setBackground(list.getSelectionBackground());
-            setForeground(list.getSelectionForeground());
-        }else{
-            setBackground(list.getBackground());
-            setForeground(list.getForeground());
-        }*/
+       
+        //PERSONALIZAR COLORES////////////////////////////////////////
         setEnabled(true);
         setFont(list.getFont());
         setOpaque(true);
@@ -60,18 +69,20 @@ public class ListaRender extends JLabel implements ListCellRenderer{
         JList.DropLocation dropLocation = list.getDropLocation();
         if (dropLocation != null
                 && !dropLocation.isInsert()
-                && dropLocation.getIndex() == index) {
-
+                && dropLocation.getIndex() == index) 
+        {
             background = Color.BLUE;
             foreground = Color.WHITE;
 
         // check if this cell is selected
-        } else if (isSelected) {
+        } else if (isSelected) 
+        {
             background = Color.BLACK;
             foreground = Color.WHITE;
 
         // unselected, and not the DnD drop location
-        } else {
+        } else 
+        {
             background = Color.WHITE;
             foreground = Color.BLACK;
         }
@@ -81,6 +92,5 @@ public class ListaRender extends JLabel implements ListCellRenderer{
         setOpaque(true);
        
         return this;
-    }
-    
+    }  
 }
